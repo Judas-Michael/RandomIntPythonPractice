18c359db46a7c823976bd1f153430d425f8a362d
JL <lollypopsandrainbows@gmail.com>
2018-01-16 10:04:05 -0600

    Instructions added

--- TitleSentencePractice.py
+++ TitleSentencePractice.py
@@ -1,23 +1,34 @@
-sentence = input("Please type a short sentence: ") #assigns sentence input to variable string
+def main():
 
-if sentence.isalnum:
-	sentence = sentence
-else: 
-	print("This sentence is not valid. Please use a-z and 0-9")
-#validates the data
+	display_banner()
+	
+	sentence = input("Please type a short sentence: ") #assigns sentence input to variable string
 
-sentence_lower = sentence.lower()
+	if sentence.isalnum:
+		sentence = sentence
+	else: 
+		print("This sentence is not valid. Please use a-z and 0-9")
+	#validates the data
+
+	sentence_lower = sentence.lower()
 
 	
-words_in_sentence = sentence_lower.split(" ") #breaks apart sentence without the ' ' characters 
+	words_in_sentence = sentence_lower.split(" ") #breaks apart sentence without the ' ' characters 
 
-#this assigns the words into a list called  words_in_sentence
-capitalized_words = [] #this creates a blank list for words
-for x in range(1, len(words_in_sentence)): #creates a loop for every word
-	word = words_in_setence[x]
-    cap_word = word[0].upper() + word[1:] #capitalizes every word
-    capitalized_words.append(cap_word) #adds word to new list
-output = ' '.join(capitalized_words)
-print(output)
+	#this assigns the words into a list called  words_in_sentence
+	capitalized_words = [] #this creates a blank list for words
+	for x in range(1, len(words_in_sentence)): #creates a loop for every word
+		word = words_in_setence[x]
+		cap_word = word[0].upper() + word[1:] #capitalizes every word
+		capitalized_words.append(cap_word) #adds word to new list
+	output = ' '.join(capitalized_words)
+	print(output)
+	
+def display_banner():
+	msg = 'AWESOME camelCaseGenerator PROGRAM'
+	stars = '*' * len(msg)
+	print('\n', stars, '\n', msg, '\n', stars, '\n')
+	print("Enter something to convert to camelCase")
+	
 
 
